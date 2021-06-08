b88bf0e6b92d5d17f12c07528b53ad368344fbf0
princemails <princemails1@gmail.com>
2021-06-08 19:32:00 +0100

    message

--- src/app/studentdetails/studentdetails.component.ts
+++ src/app/studentdetails/studentdetails.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Student } from '../student';
+
+
 
 
 @Component({
@@ -10,13 +11,30 @@ import { Student } from '../student';
 })
 export class StudentdetailsComponent implements OnInit {
   Students:any;
-  searchValue: string = '';
+  name : any;
+  p : number = 1;
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.httpClient.get('/assets/students.json').subscribe((result) => {
       this.Students = result;
     });
+  }
+
+  search(){
+    if(this.name == ""){
+      this.ngOnInit();
+    }else{
+      this.Students = this.Students.filter(result => {
+        return result.name.toLocaleLowerCase().match(this.name.toLocaleLowerCase());
+      });
+    }
+  }
+  key: string = 'name';
+  reverse: boolean = false;
+  sort(key) {
+    this.key = key;
+    this.reverse = !this.reverse;
   }
 
 }
